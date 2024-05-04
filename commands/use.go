f7728c34b6b945c455a14641b013c0b0152d88c8
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2024-05-04 11:23:14 -0400

    feat: add useFinalhandle and use command

--- commands/use.go
+++ commands/use.go
@@ -7,6 +7,47 @@ import (
 	"path/filepath"
 )
 
+var UseCmd = &cobra.Command{
+	Use:   "use",
+	Short: "Use a Neovim version",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("Error: You must specify 'nightly', a version number, or 'stable'")
+			return
+		}
+		useFinalHandler(args)
+	},
+}
+
+func useFinalHandler(args []string) {
+	switch args[0] {
+	case "nightly":
+		err := useVersion("nightly", nil)
+		if err != nil {
+			fmt.Println("Failed to use version:", err)
+			return
+		} else {
+			fmt.Println("Nightly version used successfully")
+		}
+	case "stable":
+		err := useVersion("stable", nil)
+		if err != nil {
+			fmt.Println("Failed to use version:", err)
+			return
+		} else {
+			fmt.Println("Latest stable version used successfully")
+		}
+	default:
+		err := useVersion(args[0], nil)
+		if err != nil {
+			fmt.Println("Failed to use version:", err)
+			return
+		} else {
+			fmt.Println("Version", args[0], "used successfully")
+		}
+	}
+}
+
 func useVersion(version string, optionalDir *string) error {
 	symlinkPath := "/usr/local/bin/nvim"
 
